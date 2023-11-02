c6a7caa965fdfac84b69aeb8dbb7452b1cc2846a
Correba <132346999+Correba@users.noreply.github.com>
2023-11-02 19:00:03 +0100

    Add files via upload

---
+++ MVP.py
@@ -0,0 +1,77 @@
+import argparse
+import pickle
+
+dict_enquete = dict()
+
+
+class Enquete:
+    def __init__(self, nom: str):
+        self.__nom = nom
+        self.__preuve = list()
+        self.__etat = 'En cours'
+
+    @property
+    def nom(self):
+        return self.__nom
+
+    @property
+    def preuve(self):
+        return self.__preuve
+
+    @preuve.setter
+    def preuve(self, preuve):
+        self.__preuve.append(preuve)
+
+    @property
+    def etat(self):
+        return self.__etat
+
+    @etat.setter
+    def etat(self, etat: str):
+        self.__etat = etat
+
+    def __str__(self):
+        return f'Nom: {self.__nom}, preuve: {self.__preuve}, etat: {self.__etat}'
+
+
+def save_object(obj, filename):
+    with open(filename, 'wb') as outp:  # Overwrites any existing file.
+        pickle.dump(obj, outp, pickle.HIGHEST_PROTOCOL)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', type=str, metavar='name :',
+                        help='le nom de l\'enquête a crée ou a ajouté une preuve')
+    parser.add_argument('--affichage', action='store_true',
+                        help='permet d\'afficher les enquêtes')
+    parser.add_argument('--preuve', metavar='preuve :', help='la preuve a ajouté')
+
+    args = parser.parse_args()
+
+    try:
+        with open('enquete_data', 'rb') as input_file:
+            dict_enquete = pickle.load(input_file)
+
+    except FileNotFoundError:
+        print('Fichier Introuvable')
+    except IOError:
+        print('Erreur IO')
+
+    if args.name and not args.preuve:
+        dict_enquete[args.name] = Enquete(args.name)
+        save_object(dict_enquete, 'enquete_data')
+
+    if args.preuve:
+        if args.name:
+            dict_enquete[args.name].preuve.append(args.preuve)
+        else:
+            print('Aucune enquête choisie')
+
+    if args.affichage:
+        if len(dict_enquete) != 0:
+            print(dict_enquete) #debug
+            for enquete in dict_enquete.keys():
+                print(dict_enquete[enquete])
+        else:
+            print('Aucune enquête trouver')
