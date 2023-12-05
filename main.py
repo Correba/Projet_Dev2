27a3fe1b5accd923a7e74e01d831cb575fdd4a6c
Mateo <mat.correba@gmail.com>
2023-12-05 15:13:15 +0100

    Ajout de tout les types dans les formulaires

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def create_investigations(element):
 
 
 @eel.expose
-def fill_table_investigations(args):
+def fill_table_investigations(args=None):
     read_save_file()
     html = ''
     select = ''
@@ -51,19 +51,51 @@ def fill_table_investigations(args):
 
 
 @eel.expose
-def fill_type_forms(args):
-    evidence_types = {'picture': ['location'], 'object': ['location'], 'recording': [['audio', 'video']]}
-    person_types = {'suspect': ['picture', 'suspicion', 'criminal_record'], 
-                    'culprit': ['picture', 'suspicion', 'criminal record', 'motivation', 'victim_relationship'],
-                    'witness': ['testimony', 'contact'], 'victim': ['testimony', 'contact', 'condition', 'circumstance']}
+def fill_type_forms(args=None):
+    evidence_types = ['picture', 'object', 'recording']
+    person_types = ['suspect', 'culprit', 'witness', 'victim']
     evidence_select = '<option value="default" selected><---!---></option>'
     person_select = evidence_select
-    for evidence in list(evidence_types.keys()):
+    for evidence in evidence_types:
         evidence_select += f'<option value"{evidence}">{evidence.capitalize()}</option>'
-    for person in list(person_types.keys()):
+    for person in person_types:
         person_select += f'<option value"{person}">{person.capitalize()}</option>'
     eel.addElement('evidenceType', evidence_select)
     eel.addElement('peopleType', person_select)
+
+
+@eel.expose
+def make_type_input(type: str, parent_id: str, args=None):
+    type_input = ''
+    type = type.lower()
+    return_id = parent_id.split('T')[0] + 'Extra'
+    evidence_types = {'picture': 'location', 'object': 'location', 'recording': 'recording'}
+    person_types = {'suspect': ['picture', 'suspicion', 'criminal_record'],
+                    'culprit': ['picture', 'suspicion', 'criminal_record', 'motivation', 'victim_relationship'],
+                    'witness': ['testimony', 'contact'],
+                    'victim': ['testimony', 'contact', 'condition', 'circumstance']}
+
+    if type == 'default':
+        eel.addElement(return_id, '')
+    elif 'evidence' in parent_id:
+        new_id = 'evidence' + type.capitalize()
+        type_input += f'<label for="{new_id}">{evidence_types[type].capitalize()} : </label>'
+        if type == 'recording':
+            type_input += f'<input type="file" accept="audio/*, video/*" id="{new_id}" name="{new_id}" required><br>'
+        else:
+            type_input += f'<input type="text" id="{new_id}" name="{new_id}" required><br>'
+    elif 'people' in parent_id:
+        for type_info in person_types[type]:
+            new_id = 'people' + type.capitalize() + type_info.capitalize()
+            type_input += f'<label for="{new_id}">{type_info.capitalize()} : </label>'
+            if type_info == 'picture':
+                type_input += f'<input type="file" accept="image/*" id="{new_id}" name="{new_id}" required><br>'
+            elif type_info == 'criminal_record':
+                type_input += f'<input type="file" id="{new_id}" name="{new_id}" required><br>'
+            else:
+                type_input += f'<input type="text" id="{new_id}" name="{new_id}" required><br>'
+
+    eel.addElement(return_id, type_input)
 
 
 if __name__ == '__main__':
--- web/js/main.js
+++ web/js/main.js
@@ -16,18 +16,8 @@ function addElement(id, html){
 }
 
 function setMaxDate() {
-    let today = new Date();
-    let day = today.getDate();
-    let month = today.getMonth() + 1; //January is 0!
-    let year = today.getFullYear();
-
-    if (day < 10) {
-        day = '0' + day;
-    }
-    if (month < 10) {
-        month = '0' + month;
-    }
-    document.querySelectorAll('input[type="date"]').forEach(x => x.setAttribute("max", `${year}-${month}-${day}`));
+    let today = new Date().toISOString().split('T')[0];
+    document.querySelectorAll('input[type="date"]').forEach(x => x.setAttribute("max", `${today}`));
 }
 
 /*
